ac5257b974a5e0fbc95822eac4a981a93ebe8bad
Abdur Rehman <abdurrehman345@gmail.com>
2025-11-05 22:39:29 +0300

    intial front end setup
    nitial front‑end setup with Tailwind and PostCSS,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div className="bg-primary-hauler text-white p-4">Test</div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
